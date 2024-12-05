24c15e6851b5a3c093cd0aefcfb4ba8c6190aab9
Jaden Neal <jaden.neal142@gmail.com>
2024-12-05 16:23:40 -0600

    Jokes Page

--- jokes/views.py
+++ jokes/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.views.generic import ListView
+
+from .models import Joke
+
+class JokeListView(ListView):
+    model = Joke
