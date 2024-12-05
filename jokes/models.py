b4d0c42bbe43e200240c9cd0645c6cb5d0d6ec1e
Jaden Neal <jaden.neal142@gmail.com>
2024-12-05 16:38:51 -0600

    Initial Project

--- jokes/models.py
+++ jokes/models.py
@@ -1,5 +1,6 @@
 
 from django.db import models
+from django.urls import reverse
 
 class Joke(models.Model):
     question = models.TextField(max_length=200)
@@ -7,6 +8,9 @@ class Joke(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    def get_absolute_url(self):
+        return reverse('jokes:detail', args=[str(self.pk)])
+
     def __str__(self):
         return self.question
-    
+        
