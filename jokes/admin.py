22cdff156eba4a238d00bccca615ec62a8bd3b01
Jaden Neal <jaden.neal142@gmail.com>
2024-12-06 11:45:18 -0600

    Tags

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,12 +1,12 @@
 
 from django.contrib import admin
 
-from .models import Category, Joke
+from .models import Category, Joke, Tag
 
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    model = Category
-    list_display = ['category', 'created', 'updated']
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    model = Tag
+    list_display = ['tag', 'created', 'updated']
 
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
