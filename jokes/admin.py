cbaf49c9f24aef46aea05d2c8df9c736b777d8b2
Jaden Neal <jaden.neal142@gmail.com>
2024-12-13 14:09:33 -0600

    Joke Voting

--- jokes/admin.py
+++ jokes/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 
-from .models import Category, Joke, Tag
+from .models import Category, Joke, JokeVote, Tag
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
@@ -34,4 +34,14 @@ class TagAdmin(admin.ModelAdmin):
     def get_readonly_fields(self, request, obj=None):
         if obj: # editing an existing object
             return ('slug', 'created', 'updated')
+        return ()
+    
+@admin.register(JokeVote)
+class JokeVoteAdmin(admin.ModelAdmin):
+    model = JokeVote
+    list_display = ['joke', 'user', 'vote']
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj: # editing an existing object
+            return ('created', 'updated')
         return ()
