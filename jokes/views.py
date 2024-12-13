2fb6979ddfb9527368e7b59971a8e3fb25cdf156
Jaden Neal <jaden.neal142@gmail.com>
2024-12-13 15:08:45 -0600

    Querying

--- jokes/views.py
+++ jokes/views.py
@@ -3,6 +3,7 @@ import json
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 from django.contrib.messages.views import SuccessMessageMixin
+from django.db.models import Q
 from django.http import JsonResponse
 from django.urls import reverse_lazy
 from django.views.generic import (
@@ -98,6 +99,12 @@ class JokeListView(ListView):
     def get_queryset(self):
         ordering = self.get_ordering()
         qs = Joke.objects.all()
+
+        if 'q' in self.request.GET: # Filter by search query
+            q = self.request.GET.get('q') 
+            qs = qs.filter(
+                Q(question__icontains=q) | Q(answer__icontains=q)
+            )
 
         if 'slug' in self.kwargs: # Filter by category or tag
             slug = self.kwargs['slug']
