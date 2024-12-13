7c6f7c7d3f65ba96ccb903c7196ddcba480c495a
Jaden Neal <jaden.neal142@gmail.com>
2024-12-13 15:24:42 -0600

    Querying

--- jokes/models.py
+++ jokes/models.py
@@ -1,7 +1,7 @@
 
 from django.conf import settings
 from django.db import models
-from django.db.models import Avg
+from django.db.models import Avg, Count, Sum
 from django.urls import reverse
 
 from common.utils.text import unique_slug
@@ -10,12 +10,13 @@ class Joke(models.Model):
     question = models.TextField(max_length=200)
     answer = models.TextField(max_length=100, blank=True)
     user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.PROTECT
+        settings.AUTH_USER_MODEL, on_delete=models.PROTECT,
+        related_name='jokes'
     )
     category = models.ForeignKey(
         'Category', on_delete=models.PROTECT, related_name='jokes'
     )
-    tags = models.ManyToManyField('Tag', blank=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='jokes')
     slug = models.SlugField(
         max_length=50, unique=True, null=False, editable=False
     )
@@ -33,7 +34,7 @@ class Joke(models.Model):
     @property
     def num_dislikes(self):
         return self.jokevotes.filter(vote=-1).count()
-    
+
     @property
     def rating(self):
         if self.num_votes == 0: # No jokes, so rating is 0
@@ -43,6 +44,26 @@ class Joke(models.Model):
 
         # Return the rounded rating.
         return round(5 + (r['average'] * 5), 2)
+
+    @property
+    def votes(self):
+        result = JokeVote.objects.filter(joke=self).aggregate(
+            num_votes=Count('vote'),
+            sum_votes=Sum('vote')
+        )
+
+        # If there aren't any votes yet, return a dictionary with values of 0.
+        if result['num_votes'] == 0:
+            return {'num_votes': 0, 'rating': 0, 'likes': 0, 'dislikes': 0}
+
+        # Otherwise, calculate the dict values using num_votes and sum_votes.
+        result['rating'] = round(
+            5 + ((result['sum_votes']/result['num_votes'])*5), 2
+        )
+        result['dislikes'] = int((result['num_votes'] - result['sum_votes'])/2)
+        result['likes'] = result['num_votes'] - result['dislikes']
+
+        return result
 
     def get_absolute_url(self):
         return reverse('jokes:detail', args=[self.slug])
@@ -56,26 +77,7 @@ class Joke(models.Model):
 
     def __str__(self):
         return self.question
-    
-class JokeVote(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
-        related_name='jokevotes'
-    )
-    joke = models.ForeignKey(
-        Joke, on_delete=models.CASCADE,
-        related_name='jokevotes'
-    )
-    vote = models.SmallIntegerField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=['user', 'joke'], name='one_vote_per_user_per_joke'
-            )
-        ]
 
 class Category(models.Model):
     category = models.CharField(max_length=50)
@@ -96,13 +98,11 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
-    
-    class Meta:
-        verbose_name_plural = 'Categories'
-    
+
     class Meta:
         verbose_name_plural = 'Categories'
         ordering = ['category']
+
 
 class Tag(models.Model):
     tag = models.CharField(max_length=50)
@@ -123,3 +123,24 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.tag
+
+
+class JokeVote(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='jokevotes'
+    )
+    joke = models.ForeignKey(
+        Joke, on_delete=models.CASCADE,
+        related_name='jokevotes'
+    )
+    vote = models.SmallIntegerField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=['user', 'joke'], name='one_vote_per_user_per_joke'
+            )
+        ]
